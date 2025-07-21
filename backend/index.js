5bf35067d9f98cd9b6ff30270ddf236da5a26a7b
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-07-21 12:55:01 +0800

    fix: indentation fixes index.js

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("API is running.")
-})
+    res.send("API is running.");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
