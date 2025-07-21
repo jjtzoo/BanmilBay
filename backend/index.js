fafc35a980a67a12d3b04e158cd499bdac5d3797
Jorge Jacob Toledo <bsemjtoledo@gmail.com>
2025-07-21 12:25:08 +0800

    feat: index.js

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+import connectDB from "./config/db.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running.")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
